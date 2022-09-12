f73d490b41cd0eb1c3b55f0a4700175ff1fc8ad3
Konnie Duan <kddypublic84@yahoo.com>
2022-09-12 14:44:19 -0700

    add stencils for index class

--- src/Index.java
+++ src/Index.java
@@ -1,4 +1,15 @@
 
 public class Index {
-	
+	public Index () {
+		
+	}
+	public void init() {
+		
+	}
+	public void add (String filename) {
+		
+	}
+	public void remove (String filename) {
+		
+	}
 }
